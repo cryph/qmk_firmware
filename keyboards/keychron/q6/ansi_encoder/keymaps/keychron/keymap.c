ff6c7a302e2cab0c96b7156404361014ba874894
Cryph <cryph@gmail.com>
2023-05-27 19:14:29 -0700

    Double-Use Encoder Click, Mute 1.5s delay Play/Pause

--- keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
+++ keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
@@ -27,6 +27,24 @@ enum layers{
   DIABLO
 };
 
+//Tap Dance Declarations
+enum {
+  TD_MUTE_PLAY = 0
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_MUTE_PLAY]  = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, KC_MPLY),
+};
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TD(TD_MUTE_PLAY):
+            return TAPPING_TERM + 1069;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_109(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    KC_SNAP,  KC_SIRI,  RGB_MOD,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
@@ -43,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
     [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CRTA,  RGB_MOD,  _______,  _______,  _______,  _______,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     TD(TD_MUTE_PLAY),    KC_PSCR,  KC_CRTA,  RGB_MOD,  _______,  _______,  _______,  _______,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
@@ -53,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  TG(DIABLO),  RGB_TOG,  DM_REC1,  DM_REC2,  DM_PLY1,  DM_PLY2,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  KC_BTN1,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              KC_MS_U,            _______,  _______,  _______,
-        _______,  0x700b,  _______,                                _______,                                _______,  KC_BTN1,  _______,    _______,    KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,            _______,  _______),
+        _______,  0x700b,  _______,                                _______,                                _______,  _______,  _______,    _______,    KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,            _______,  _______),
     [DIABLO] = LAYOUT_ansi_109(
         _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,     _______,    _______,  _______,  RGB_MOD,  _______,  _______,  _______,  _______,
         _______,   _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,     _______,    _______,   _______,  _______,  _______,   _______,  _______,  _______,
@@ -81,10 +99,10 @@ void housekeeping_task_user(void) {
     housekeeping_task_keychron();
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if(!process_record_keychron(keycode, record)) {
-        return false;
-    }
-
-    return true;
-}
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    if(!process_record_keychron(keycode, record)) {
+//        return false;
+//    }
+//
+//    return true;
+//}
