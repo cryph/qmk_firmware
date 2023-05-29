02c666c4bf8b1edb63a1f32b601a02e62fbf76b7
Cryph <cryph@gmail.com>
2023-05-29 13:01:46 -0700

    Diablo Layer Rearrange, few outstanding bugs, lot of whitespace mess

--- keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
+++ keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
@@ -23,18 +23,9 @@ enum layers{
   MAC_BASE,
   MAC_FN,
   WIN_BASE,
-  WIN_FN,
-  DIABLO
+  DIABLO,
+  WIN_FN
 };
-
-//Tap Dance Declarations
-//enum {
-//  TD_MUTE_PLAY = 0
-//};
-
-//tap_dance_action_t tap_dance_actions[] = {
-//    [TD_MUTE_PLAY]  = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, KC_MPLY),
-//};
 
 #include "tap_dances.c"
 
@@ -63,12 +54,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
     [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     LB_MUTE_PLAY,    KC_PSCR,  KC_CRTA,  RGB_MOD,  _______,  _______,  _______,  _______,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     LB_MUTE_PLAY,    KC_PSCR,  KC_SCRL,  RGB_MOD,  _______,  _______,  _______,  _______,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),    
+    [DIABLO] = LAYOUT_ansi_109(
+        _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,     _______,    _______,  _______,  RGB_MOD,  KC_D4_1,  KC_D4_2,  KC_D4_3,  KC_D4_4,
+        _______,   _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,     _______,    _______,   _______,  _______,  _______,   _______,  _______,  _______,
+        _______,   _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,    _______,    _______,   _______,   _______,  _______,    _______,    _______,
+        _______,  _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,              _______,                                   _______,    _______,    _______,    _______,
+        _______,            _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,   _______,              _______,              _______,              _______,    _______,    _______,
+        _______,  _______,  _______,                                _______,                                 _______,  _______,  MO(WIN_FN), _______,    _______,  _______,  _______,  _______,              _______,  _______),
     [WIN_FN] = LAYOUT_ansi_109(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  TG(DIABLO),  RGB_TOG,  DM_REC1,  DM_REC2,  DM_PLY1,  DM_PLY2,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
@@ -76,22 +74,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  KC_BTN1,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              KC_MS_U,            _______,  _______,  _______,
         _______,  0x700b,  _______,                                _______,                                _______,  _______,  _______,    _______,    KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,            _______,  _______),
-    [DIABLO] = LAYOUT_ansi_109(
-        _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,     _______,    _______,  _______,  RGB_MOD,  KC_D4_1,  KC_D4_2,  KC_D4_3,  KC_D4_4,
-        _______,   KC_1,     KC_2,     KC_3,     KC_4,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,     _______,    _______,   _______,  _______,  _______,   _______,  _______,  _______,
-        _______,   _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,    _______,    _______,   _______,   _______,  _______,    _______,    _______,
-        _______,  _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,              _______,                                   _______,    _______,    _______,    _______,
-        _______,            _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,  _______,   _______,              _______,              _______,              _______,    _______,    _______,
-        _______,  _______,  _______,                                _______,                                 _______,  _______,  MO(WIN_FN), _______,    _______,  _______,  _______,  _______,              _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },    
+    [DIABLO]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [DIABLO]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
 
