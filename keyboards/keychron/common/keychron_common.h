99b6a0e2dcb8e722db7809d05361d7747faa9289
Cryph <cryph@gmail.com>
2023-05-31 16:34:23 -0700

    BT-500 Enable/Disable Macros on Base Layer

--- keyboards/keychron/common/keychron_common.h
+++ keyboards/keychron/common/keychron_common.h
@@ -35,7 +35,9 @@ enum custom_keycodes {
     KC_TASK_VIEW,
     KC_FILE_EXPLORER,
     KC_SCREEN_SHOT,
-    KC_CORTANA
+    KC_CORTANA,
+    LBMACRO1,
+    LBMACRO2
 };
 
 #define KC_TASK KC_TASK_VIEW
--- keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
+++ keyboards/keychron/q6/ansi_encoder/keymaps/keychron/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
     [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     LB_MUTE_PLAY,    KC_PSCR,  KC_SCRL,  RGB_MOD,  _______,  _______,  _______,  _______,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     LB_MUTE_PLAY,    KC_PSCR,  KC_SCRL,  RGB_MOD,  LBMACRO1,  LBMACRO2,  _______,  _______,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
@@ -92,10 +92,47 @@ void housekeeping_task_user(void) {
     housekeeping_task_keychron();
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if(!process_record_keychron(keycode, record)) {
-        return false;
-    }
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    if(!process_record_keychron(keycode, record)) {
+//        return false;
+//    }
 
+//    return true;
+//}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case LBMACRO1:
+        if (record->event.pressed) {
+            // when keycode LBMACRO1 is pressed
+            SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_TAP(X_PSCR));
+            SEND_STRING(SS_UP(X_LCTL)SS_UP(X_LALT));
+            SEND_STRING(SS_DELAY(300));
+            SEND_STRING("timer 1 period 199\n");
+            SEND_STRING(SS_DELAY(50));
+            SEND_STRING("save\n");
+            SEND_STRING(SS_DELAY(25));
+            SEND_STRING("exit\n");
+        } else {
+            // when keycode LBMACRO1 is released
+        }
+        break;
+		
+    case LBMACRO2:
+        if (record->event.pressed) {
+            // when keycode LBMACRO2 is pressed
+            SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_TAP(X_PSCR));
+            SEND_STRING(SS_UP(X_LCTL)SS_UP(X_LALT));
+            SEND_STRING(SS_DELAY(300));
+            SEND_STRING("timer 1 period 0\n");
+            SEND_STRING(SS_DELAY(50));
+            SEND_STRING("save\n");
+            SEND_STRING(SS_DELAY(25));
+            SEND_STRING("exit\n");
+        } else {
+            // when keycode LBMACRO2 is released
+        }
+        break;
+    }
     return true;
-}
+};
